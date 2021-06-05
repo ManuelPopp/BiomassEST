02e3920b42897d39b399889d02cc0e557687fc95
Manuel Popp <34488159+ManuelPopp@users.noreply.github.com>
2021-06-05 09:47:45 +0200

    Fixed Errors

--- R/est_bm.R
+++ R/est_bm.R
@@ -31,11 +31,11 @@ est.bm <- function(Species, RCD = NA, BHD = NA, H = NA, use = NA, H_meas = NA){
       AGB <- vector()
       for(i in 1:length(Species)){
         if(use[i] == "RCD"){
-          AGB[i] <- BiomassEST::est.bm.RCD(Species = Species, RCD = RCD, H_meas = H_meas)
+          AGB[i] <- BiomassEST::est.bm.RCD(Species = Species[i], RCD = RCD[i], H_meas = H_meas[i])
         }else if(use[i] == "H"){
-          AGB[i] <- BiomassEST::est.bm.H(Species = Species, H = H)
+          AGB[i] <- BiomassEST::est.bm.H(Species = Species[i], H = H[i])
         }else if(is.na(use[i]) | use == "RCD2H"){
-          AGB[i] <- BiomassEST::est.bm.RCD2H(Species = Species, RCD = RCD, H = H, H_meas = H_meas)
+          AGB[i] <- BiomassEST::est.bm.RCD2H(Species = Species[i], RCD = RCD[i], H = H[i], H_meas = H_meas[i])
         }
       }
     }
